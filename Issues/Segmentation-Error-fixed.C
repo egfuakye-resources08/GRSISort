4b9334c2a821b9ca47af1196d95540f8d525cba0
Eric Fuakye <75108172+egfuakye-resources08@users.noreply.github.com>
2021-01-11 09:13:13 -0600

    Update Segmentation-Error-fixed.C

--- Issues/Segmentation-Error-fixed.C
+++ Issues/Segmentation-Error-fixed.C
@@ -1,15 +1,20 @@
-TO FIX THE CRUSH IN GRSIPROOF which is SEGMENTATION ERROR; 
-FOLLOW THE FOLLOWING STEPS 
-STEP 1
+#TO FIX THE CRUSH IN GRSIPROOF which is SEGMENTATION ERROR; 
+
+#1. INSTALL VALGRIND
 
 sudo apt-get update
 sudo apt-get install valgrind
 
-STEP 2 
+#2. HOW TO APPLY VALGRIND ON YOUR PROGRAM
 
 G_SLICE=always-malloc G_DEBUG=gc-friendly  valgrind -v --tool=memcheck --leak-check=full --num-callers=40 
 
 --log-file=valgrind.log grsiproof Name-of-fragment-file  EnergyFragmentSelector.C
+
+#3. CHECK OUT FINALLY
+
+G_SLICE=always-malloc G_DEBUG=gc-friendly  valgrind -v --tool=memcheck --leak-check=full --num-callers=40 
+--log-file=valgrind.log grsiproof /home/cern33303/GRSISort/GRSIProof/ExampleEventSelector.C --addback-window 300 fragment10484_???.root 
 
 
 
