e314aaccf1bac6bc480aca1cc87c54befb6ff71f
Eric Fuakye <75108172+egfuakye-resources08@users.noreply.github.com>
2021-01-10 23:49:46 -0600

    Add files via upload

--- Issues/Segmentation-Error-fixed.C
+++ Issues/Segmentation-Error-fixed.C
@@ -1,1 +1,16 @@
+TO FIX THE CRUSH IN GRSIPROOF which is SEGMENTATION ERROR; 
+FOLLOW THE FOLLOWING STEPS 
+STEP 1
+
+sudo apt-get update
+sudo apt-get install valgrind
+
+STEP 2 
+
+G_SLICE=always-malloc G_DEBUG=gc-friendly  valgrind -v --tool=memcheck --leak-check=full --num-callers=40 --log-file=valgrind.log grsiproof Name-of-fragment-file  EnergyFragmentSelector.C
+
+
+
+
+
 
